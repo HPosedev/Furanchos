c7e8f2c1c222818261d5bef705c7e3ff54074131
Hector <hposedev@gmail.com>
2022-04-07 21:01:23 +0200

    added toolbar and updated layout

--- app/src/main/java/com/hposedev/furanchos/MainActivity.kt
+++ app/src/main/java/com/hposedev/furanchos/MainActivity.kt
@@ -15,6 +15,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setSupportActionBar(binding.myToolbar)
         initRecyclerView()
     }
     private fun initRecyclerView(){
